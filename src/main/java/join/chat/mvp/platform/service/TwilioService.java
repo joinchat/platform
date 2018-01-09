f2129c5f8366806f287633d28cc478785d136503
Evgenii Zhuravlev <e.zhuravlev@cybsec.org>
2018-01-09 18:19:03 +0200

    Added support localization pattern

--- src/main/java/join/chat/mvp/platform/service/TwilioService.java
+++ src/main/java/join/chat/mvp/platform/service/TwilioService.java
@@ -7,28 +7,43 @@ import join.chat.mvp.platform.configuration.TwilioConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.util.Locale;
 import java.util.concurrent.CompletableFuture;
 
 @Service
 public class TwilioService {
     private static final Logger logger = LoggerFactory.getLogger(TwilioService.class);
+
+    private final MessageSource messageSource;
     private final TwilioConfiguration configuration;
 
     @Autowired
-    public TwilioService(final TwilioConfiguration configuration) {
+    public TwilioService(final MessageSource messageSource, final TwilioConfiguration configuration) {
+
+        this.messageSource = messageSource;
         this.configuration = configuration;
+
         Twilio.init(this.configuration.getAccountSid(), this.configuration.getAuthToken());
+        logger.info("Twilio initialized with SID: {0} and token: {1}",
+                this.configuration.getAccountSid(), this.configuration.getAuthToken());
     }
 
     @Async
-    public CompletableFuture<String> getVerificationCode(final String number, final String code)
+    public CompletableFuture<String> pushVerificationCode(final String number, final String code)
+            throws InterruptedException {
+        return pushVerificationCode(number, code, null);
+    }
+
+    @Async
+    public CompletableFuture<String> pushVerificationCode(final String number, final String code, final Locale locale)
             throws InterruptedException {
         final String phoneFrom = this.configuration.getPhoneNumber();
         final Message message = Message.creator(new PhoneNumber(number), new PhoneNumber(phoneFrom),
-                code + " is your Join.Chat verification code").create();
+                code + " " + messageSource.getMessage("validation_code", null, locale)).create();
 
         logger.info("Sending verification code: {0} to {1} from {2}", code, number, phoneFrom);
         return CompletableFuture.completedFuture(message.getSid());
